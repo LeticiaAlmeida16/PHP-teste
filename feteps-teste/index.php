4119c43212a115ded31db7d787daf59d5a2d068d
Letícia Nascimento de Almeida <124791668+LeticiaAlmeida16@users.noreply.github.com>
2023-10-03 18:24:30 -0300

    Add files via upload

---
+++ feteps-teste/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Meu 1º teste</title>
+</head>
+<body>
+    <?php
+        echo "<h1> Hello world! </h1>";
+    
+    ?>
+</body>
+</html>
+
